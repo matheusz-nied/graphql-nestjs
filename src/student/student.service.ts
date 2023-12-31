2eb39cf13cd875499a7619944b2d4973667c5b6e
matheusz nied <matheustae@hotmail.com>
2023-12-31 08:47:21 -0300

    feat: implements student service

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Student } from './student.entity';
+import { Repository } from 'typeorm';
+import { CreateStudentInput } from './create-student.input';
+import { v4 as uuid } from 'uuid';
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(
+    @InjectRepository(Student)
+    private studentRepository: Repository<Student>,
+  ) {}
+
+  async getStudent(id: string): Promise<Student> {
+    return await this.studentRepository.findOne({ where: { id } });
+  }
+
+  async getAllStudents(): Promise<Student[]> {
+    return await this.studentRepository.find();
+  }
+
+  async createStudent(createStudentInput: CreateStudentInput) {
+    const { name, lastName } = createStudentInput;
+
+    const student = this.studentRepository.create({
+      id: uuid(),
+      name,
+      lastName,
+    });
+
+    return await this.studentRepository.save(student);
+  }
+}
